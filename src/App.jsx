67f843272685be427c85bedd7237b5594268c061
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2023-08-07 23:16:19 +0600

    getting started with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: blanchedalmond;
+`;
+
+const StyledApp = styled.div`
+  background-color: black;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The World Oasis</H1>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
